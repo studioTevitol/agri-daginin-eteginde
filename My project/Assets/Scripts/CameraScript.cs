2b184a37560f497bd3eddb39b5b5c1d3460a64f4
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-03-19 17:45:20 +0300

    Add CameraScript for smooth player tracking

---
+++ My project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed=1f;
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start(){
+        transform.position = new Vector3(player.transform.position.x,player.transform.position.y,transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float dx = player.transform.position.x - transform.position.x;
+        float dy = player.transform.position.y - transform.position.y;
+        if(Mathf.Sqrt(dx*dx+dy*dy)<=maxSpeed*Time.deltaTime){
+            transform.position = new Vector3(player.transform.position.x,player.transform.position.y,transform.position.z);
+        }
+        else{
+            float angle = Mathf.Atan2(dy,dx);
+            transform.position = new Vector3(transform.position.x+maxSpeed*Time.deltaTime*Mathf.Cos(angle),transform.position.y+maxSpeed*Time.deltaTime*Mathf.Sin(angle),transform.position.z);
+        }
+    }
+}
