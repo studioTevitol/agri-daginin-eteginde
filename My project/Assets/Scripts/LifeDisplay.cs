30b79853bfbc085777f2bdab3827f49b6309324b
theko1 <cakirardaekrem@gmail.com>
2024-05-15 22:50:16 +0300

    have fixed life count and colour of the text mesh pro -even tough branch name is indicating that i should be fixing jump mechanic-

--- My project/Assets/Scripts/LifeDisplay.cs
+++ My project/Assets/Scripts/LifeDisplay.cs
@@ -13,6 +13,12 @@ public class LifeDisplay : MonoBehaviour
     [SerializeField] private List<Color> colors;
 
     void Start(){
+        //Editorden arraye color ekleyince "çekemiyordu" rengi direkt buradan atadım
+        Color thirdLifeColor = new Color(210,0,0);  colors.Add(thirdLifeColor);
+        Color secondLifeColor = new Color(255,220,0); colors.Add(secondLifeColor);
+        Color firstLifeColor = new Color(0,210,0); colors.Add(firstLifeColor);
+
+
         if(lifeText == null )Debug.Log("lifeText is null");
         if(playerLife == null)Debug.Log("playerLife is null");
         if(playerLife.life == 0)Debug.Log("playerLife.life is null");
@@ -23,7 +29,9 @@ public class LifeDisplay : MonoBehaviour
         if(playerLife.life <= 0){
             youDied.enabled = true;
         }
+
         lifeText.text=playerLife.life.ToString();
-        lifeText.color = colors[playerLife.life];
+        lifeText.color = colors[playerLife.life-1];
     }
+
 }
