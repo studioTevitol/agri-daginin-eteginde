9ece3cda59cd5ceb2b75ca6e787f38ebd8ccdd63
Teoman Ata Korkmaz <teoata17@gmail.com>
2024-06-05 17:14:25 +0300

    commit didnt work

---
+++ My project/Assets/Scripts/cc_MovingPlatform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cc_MovingPlatform : MonoBehaviour
+{
+    private Vector3 _startPos;
+    [SerializeField] private float speed=2.5f,length=5.0f;
+    private float cycle=0.0f;
+
+    void Start(){
+        _startPos=transform.position;
+    }
+
+    void Update(){
+        transform.position=new Vector3(_startPos.x-Mathf.Sin(cycle)*length,_startPos.y+Mathf.Cos(cycle)*length,_startPos.z);
+        cycle+=speed*Time.deltaTime/length;
+    }
+}
