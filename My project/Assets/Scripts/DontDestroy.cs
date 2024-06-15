68a1cc6f93f5d0bdf523f6943270a2b7f237ce05
theko1 <cakirardaekrem@gmail.com>
2024-06-15 12:51:25 +0300

    after several fuckups i've tried to merge it by hand. ofc commit histories and etc. will be kinda shitty but it is what it is. will do better job next time

---
+++ My project/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+    public bool isOnMenus; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.GameObject());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(SceneManager.GetActiveScene().name);
+        Debug.Log(isOnMenus);
+        if (SceneManager.GetActiveScene().name == "MainMenu" || SceneManager.GetActiveScene().name == "Credits")
+        {
+            isOnMenus = true;
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else
+        {
+            isOnMenus = false;
+            transform.GetChild(0).gameObject.SetActive(true);
+        }       
+    }
+}
